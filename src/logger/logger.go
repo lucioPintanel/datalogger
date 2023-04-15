62cd3791f16227032e36255c8c81e242c344d8c7
Lucio Pintanel <lm.pintanel@gmail.com>
2023-04-15 20:22:17 -0300

    - Adicionado constante de nível de registro.

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -1,1 +1,17 @@
 package logger
+
+// LogLevel type
+type LogLevel int32
+
+const (
+	//TRACE logs everything
+	TRACE LogLevel = 1 << iota
+	//DEBUG logs Debug, Info, Warnings and Errors
+	DEBUG
+	//INFO logs Info, Warnings and Errors
+	INFO
+	//WARN logs Warning and Errors
+	WARN
+	//ERROR logs just Errors
+	ERROR
+)
